6985c938d4e6f4eb70abb78761de87dad557b6b0
Susanna Casarini <104819186+SusannaCasarini@users.noreply.github.com>
2026-01-07 19:35:07 +0100

    Update RidgelinePlot.R

--- RidgelinePlot.R
+++ RidgelinePlot.R
@@ -44,9 +44,9 @@ im.ridgelinecrop <- function(im, scale, palette = c(
   df_list <- list()
   
   for (i in seq_along(bands)) {
-    df <- as.data.frame(bands[[i]]) %>%
+    df <- as.data.frame(bands[[i]], wide=FALSE) %>%
       pivot_longer(
-        cols= everything(),
+        cols= -c(wide),
         names_to = "layer",
         values_to = "values"
       ) %>%
